cf993dd30ce8700e7689d733952802b8b717a4a4
njugus <kayvohnjuguna@gmail.com>
2024-07-02 12:34:12 -0700

    adds responsiveness to the Top specialists section

--- client/src/Page/Home/Specialist.jsx
+++ client/src/Page/Home/Specialist.jsx
@@ -24,10 +24,10 @@ export function Specialists() {
                         <h2 className="name-occupation">Psychologist</h2>
                     </div>
                     <div className="specialist-media-handles">
-                        <FaFacebook/>
-                        <FaInstagramSquare/>
-                        <FaLinkedin/>
-                        <FaXTwitter/>
+                        <a href='https://www.facebook.com' target='_blank'><FaFacebook/></a>
+                        <a href="https://www.instagram.com" target="_blank" ><FaInstagramSquare/></a>
+                        <a href="https://www.linkedin.com" target="_blank" ><FaLinkedin/></a>
+                        <a href="https://www.twitter.com" target="_blank" ><FaXTwitter/></a>
                     </div>
                 </div> 
                 <div className="specialist-card">
@@ -35,14 +35,14 @@ export function Specialists() {
                         <img src={specTwo} className="specialist-img"/>
                     </div>
                     <div className="specialist-title">
-                        <h2 className="name-item">Dr Aisha</h2>
+                        <h2 className="name-item">Dr Anna</h2>
                         <h2 className="name-occupation">Psychologist</h2>
                     </div>
                     <div className="specialist-media-handles">
-                        <FaFacebook/>
-                        <FaInstagramSquare/>
-                        <FaLinkedin/>
-                        <FaXTwitter/>
+                        <a href='https://www.facebook.com' target='_blank'><FaFacebook/></a>
+                        <a href="https://www.instagram.com" target="_blank" ><FaInstagramSquare/></a>
+                        <a href="https://www.linkedin.com" target="_blank" ><FaLinkedin/></a>
+                        <a href="https://www.twitter.com" target="_blank" ><FaXTwitter/></a>
                     </div>
                 </div> 
                 <div className="specialist-card">
@@ -50,14 +50,14 @@ export function Specialists() {
                         <img src={specThree} className="specialist-img"/>
                     </div>
                     <div className="specialist-title">
-                        <h2 className="name-item">Dr Aisha</h2>
+                        <h2 className="name-item">Dr Francis</h2>
                         <h2 className="name-occupation">Psychologist</h2>
                     </div>
                     <div className="specialist-media-handles">
-                        <FaFacebook/>
-                        <FaInstagramSquare/>
-                        <FaLinkedin/>
-                        <FaXTwitter/>
+                        <a href='https://www.facebook.com' target='_blank'><FaFacebook/></a>
+                        <a href="https://www.instagram.com" target="_blank" ><FaInstagramSquare/></a>
+                        <a href="https://www.linkedin.com" target="_blank" ><FaLinkedin/></a>
+                        <a href="https://www.twitter.com" target="_blank" ><FaXTwitter/></a>
                     </div>
                 </div>
                 <div className="specialist-card">
@@ -65,7 +65,7 @@ export function Specialists() {
                         <img src={specFour} className="specialist-img"/>
                     </div>
                     <div className="specialist-title">
-                        <h2 className="name-item">Dr Aisha</h2>
+                        <h2 className="name-item">Dr George</h2>
                         <h2 className="name-occupation">Psychologist</h2>
                     </div>
                     <div className="specialist-media-handles">
