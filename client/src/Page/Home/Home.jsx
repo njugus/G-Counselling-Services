e3c3be3a9c741ff9d7f0382c2c64bd9fa8d904af
njugus <kayvohnjuguna@gmail.com>
2024-07-02 10:49:31 -0700

    adds the numbers section

--- client/src/Page/Home/Home.jsx
+++ client/src/Page/Home/Home.jsx
@@ -8,6 +8,7 @@ function Home () {
         <>
         <Hero />
         <About />
+        <Numbers />
         </>
     )
 }
--- client/src/Page/Home/Numbers.jsx
+++ client/src/Page/Home/Numbers.jsx
@@ -3,6 +3,26 @@ import './Numbers.css';
 function Numbers() {
     return (
         <>
+        <section className="numbers-section">
+            <div className="numbers-wrapper">
+                <div className="item">
+                    <h1>6250+</h1>
+                    <p>Happy Customers</p>
+                </div>
+                <div className="item">
+                     <h1>3200+</h1>
+                     <p>Successfull Therapies</p>
+                </div>
+                <div className="item">
+                     <h1>20+</h1>
+                     <p>Years Of Expirience</p>
+                </div>
+                <div className="item">
+                    <h1>20+</h1>
+                    <p>Specialists</p>
+                </div>
+            </div>
+        </section>
         </>
     )
 }
